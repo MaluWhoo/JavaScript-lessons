ef6659b04af408283382ac7fe2bce4fc31e35e69
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-09-23 20:35:35 -0300

    Add files via upload
    Add some files

---
+++ outros.js
@@ -0,0 +1,16 @@
+// Instrução -> Uma maneira de enviar comandos para o computador
+
+console.log("Olá, Mundo!"); // Exibe uma mensagem no console
+
+// Bloco de Código
+
+{ // Utilizados para repetição, condicionais, funções, etc.
+    console.log("Este é um bloco de código.");
+} // ; (ponto e vírgula) é utilizado somente em instruções
+
+// Comentários e Anotações 
+
+    // (duas barras) -> Comentário de uma linha
+    /* (barra e asterisco) -> Comentário de múltiplas linhas */
+
+    
---
+++ variaveis.js
@@ -0,0 +1,28 @@
+// Cálculo IMC
+
+let altura = 1.70
+let peso = 70 
+
+const pi = 3.14 // variável constante
+
+let r = peso / (altura * altura)
+
+let teste = pi * 2
+
+console.log("O resultado é: " + r)
+console.log("O resultado é: " + teste) 
+
+/*
+
+    Renomenado variaveis:
+        1. Não inicar com número, apenas letras ou _
+        2. Não usar espaços
+        3. Não usar caracteres especiais, apenas _ e $
+        4. Não usar palavras reservadas (var, let, const, function, if, else, etc)
+
+
+        5. Usar camelCase (alturaPessoa, pesoPessoa, etc)
+        6. Variáveis são case sensitive (altura é diferente de Altura)
+
+*/
+
